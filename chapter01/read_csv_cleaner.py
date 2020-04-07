b090be25d865d676039c1535fec6beb89d734263
srutis <sruti_2003@yahoo.com>
2020-04-07 13:30:33 -0400

    Removed possibly confusing/misunderstood redundant var, clarified output.

--- chapter01/read_csv_cleaner.py
+++ chapter01/read_csv_cleaner.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def process_salary(csv_reader):
     """Process salary of user from csv file."""
     line_count = 0
@@ -8,12 +7,11 @@ def process_salary(csv_reader):
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
-        print(f'\t{row["name"]} salary: {row["salary"]}')
+        print(f'\t{row["name"]}\'s salary: {row["salary"]}')
         line_count += 1
     print(f'Completed {line_count} lines.')
 
 
 with open('employee.txt', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    line_count = 0
     process_salary(csv_reader)
